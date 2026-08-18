9189720c39d8a56c0d2ae2ec96364a0974c581d7
Vexol-files <wolskidawid07@gmail.com>
2026-08-18 17:30:40 +0200

    Update off.lua

--- off.lua
+++ off.lua
@@ -1,3 +1,3 @@
 local Players = game:GetService("Players")
 local lp = Players.LocalPlayer
-lp:Kick("The script has been deleted by its owner.")
+lp:Kick("The script has been disabled. \n Try later")
